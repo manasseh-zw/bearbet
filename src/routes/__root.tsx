0b3b308d1d1aa7b190b2c599f4e4a1be055af9fc
manasseh-zw <manassehtchanga@gmail.com>
2026-09-10 13:41:30 +0200

    feat: install BearBet favicon assets

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -17,10 +17,38 @@ export const Route = createRootRouteWithContext<AppContext>()({
 				content: "width=device-width, initial-scale=1",
 			},
 			{
+				name: "apple-mobile-web-app-title",
+				content: "BearBet",
+			},
+			{
 				title: "Bearbet",
 			},
 		],
 		links: [
+			{
+				rel: "icon",
+				type: "image/png",
+				href: "/favicon-96x96.png",
+				sizes: "96x96",
+			},
+			{
+				rel: "icon",
+				type: "image/svg+xml",
+				href: "/favicon.svg",
+			},
+			{
+				rel: "shortcut icon",
+				href: "/favicon.ico",
+			},
+			{
+				rel: "apple-touch-icon",
+				sizes: "180x180",
+				href: "/apple-touch-icon.png",
+			},
+			{
+				rel: "manifest",
+				href: "/site.webmanifest",
+			},
 			{
 				rel: "stylesheet",
 				href: appCss,
